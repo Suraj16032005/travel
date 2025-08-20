891d5f67d4f929030fc37cb1a46b87f6daa2d3fc
Suraj Nair <snair4577@gmail.com>
2025-08-20 21:21:22 +0530

    inside edit listing, instead of showing the url, the option to add a new picture is shown

--- controller/listing.js
+++ controller/listing.js
@@ -2,7 +2,6 @@ const Listing=require("../models/listing");
 
 module.exports.index= async (req, res) => {
     const alllistings = await Listing.find();
-    // console.log(alllistings);
     res.render("listings/alllistings.ejs", { alllistings });
 };
 
